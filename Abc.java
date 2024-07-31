51f2adff1ee6e1446e1ccd4cd3c1fdb600e833ce
Jssca4 <jessica_ristiani_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:49:47 +0700

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,9 @@
+public class Abc {
+    public static void main(String[] args) {
+        System.out.println("Halo");
+        System.out.println("Nama saya Jessica Ristiani");
+        System.out.println("Alamat rumah saya di Ds. Setonorejo-Kras-Kediri Jawa Timur");
+        System.out.println("Saya dulu bersekolah di MTsN 2 KOTA KEDIRI");
+        System.out.println("No hp 081252529385");
+    }
+}
